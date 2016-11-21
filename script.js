59617079dab0e9a0f608c052c4c06f8c6b864bbe
Thomas Perez <thomasperez@uagateway.org>
2016-11-21 21:32:42 +0000

    I changed the html and css of my portfolio page

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
-      $("#like").html("You like this project!");
+      $(this).next().html("You like this project!");
     });
-    
-    
+          
     
     
 });
